34259446ad58cd6520263cc8ecdadcf0e1fb2b05
github username <github email address>
2018-10-12 13:51:44 -0400

    Rails routes for CRD are working

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,9 +1,34 @@
 class Api::V1::ProjectsController < ApplicationController
+  before_action :find_project, only: [:update]
 
     def index
       @projects = Project.all
       render json: @projects
     end
+
+    def create
+        @project = Project.create(project_params)
+        # byebug
+        if @project.save
+          render json: @project, status: :accepted
+        else
+          # byebug
+          render json: { errors: @project.errors.full_messages }, status: :unprocessible_entity
+        end
+      end
+
+      def update
+        @project.update(project_params)
+        if @project.save
+          render json: @project, status: :accepted
+        else
+          render json: { errors: @project.errors.full_messages }, status: :unprocessible_entity
+        end
+      end
+
+      def destroy
+        render json: Project.find(params[:id]).destroy
+      end
 
     def update
       @project.update(project_params)
@@ -24,7 +49,7 @@ class Api::V1::ProjectsController < ApplicationController
       # :coverImage_id
     end
 
-    def find_note
+    def find_project
       @project = Project.find(params[:id])
     end
 
