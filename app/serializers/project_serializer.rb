76e34ed71a9c81b5d08d112cd7e603207c6e8599
github username <github email address>
2018-10-20 18:52:37 -0400

    very minor housekeeping

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,5 +1,5 @@
 class ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :status, :start_date, :images
+  attributes :id, :title, :description, :status, :created_at, :updated_at, :images
   has_many :steps
   has_many :images, through: :steps
 end
