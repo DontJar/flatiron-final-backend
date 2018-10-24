810d5b6989e6c568d16c312b57d663c8cd7e67cd
github username <github email address>
2018-10-24 12:59:25 -0400

    minor backend changes to make image uploads resize

--- app/models/image.rb
+++ app/models/image.rb
@@ -4,6 +4,13 @@ class Image < ApplicationRecord
 
   def smaller_img
     return self.step_image.variant(resize: '750X750')
+    #
+    # @new_img.combine_options do |b|
+    #   b.resize '750x750'
+    #   b.format "png"
+    # end
+    #
+    # return @new_img
   end
 
 end
